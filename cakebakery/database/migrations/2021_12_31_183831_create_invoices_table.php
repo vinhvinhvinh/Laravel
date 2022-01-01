ba086e37f643ff721d307dc470acb72827b56c87
Vinh <ntvinh0606@gmail.com>
2022-01-01 21:15:29 +0700

    Model, migration

---
+++ cakebakery/database/migrations/2021_12_31_183831_create_invoices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('matk');
+            $table->date('ngaylaphd');
+            $table->string('nvlap');
+            $table->integer('tongtien');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}
---
+++ cakebakery/database/migrations/2021_12_31_184050_create_menu_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenuDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu_details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('mamenu');
+            $table->string('mabanh');
+            $table->integer('soluong');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu_details');
+    }
+}
